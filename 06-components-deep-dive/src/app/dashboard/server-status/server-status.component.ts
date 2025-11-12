dd2a7f191e1a7caceba26b623537639a901d2e5e
Dave March <dbmarch@gmail.com>
2025-11-11 23:22:14 -0500

    using effect with signals

--- 06-components-deep-dive/src/app/dashboard/server-status/server-status.component.ts
+++ 06-components-deep-dive/src/app/dashboard/server-status/server-status.component.ts
@@ -1,16 +1,22 @@
-import { Component, DestroyRef, OnInit, inject } from '@angular/core';
+import { Component, DestroyRef, OnInit, inject, signal, effect} from '@angular/core';
 
 @Component({
     selector: 'app-server-status',
+    standalone: true,
     imports: [],
     templateUrl: './server-status.component.html',
     styleUrl: './server-status.component.css'
 })
 export class ServerStatusComponent implements OnInit {
-  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+  // currentStatus: 'online' | 'offline' | 'unknown' = signal('offline');
+  currentStatus = signal<'online' | 'offline' | 'unknown'> ('offline');
   private destroyRef = inject(DestroyRef);
 
-  constructor() {}
+  constructor() {
+    effect(()=>{
+      console.log('server-status', this.currentStatus())
+    })
+  }
 
   ngOnInit() {
     console.log('ON INIT');
@@ -18,11 +24,11 @@ export class ServerStatusComponent implements OnInit {
       const rnd = Math.random(); // 0 - 0.99999999999999
 
       if (rnd < 0.5) {
-        this.currentStatus = 'online';
+        this.currentStatus.set('online');
       } else if (rnd < 0.9) {
-        this.currentStatus = 'offline';
+        this.currentStatus.set('offline');
       } else {
-        this.currentStatus = 'unknown';
+        this.currentStatus.set('unknown');
       }
     }, 5000);
 
