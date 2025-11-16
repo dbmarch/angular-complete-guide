ffe68c6b89ef4cb9b2f888c273104cd99b3c8700
Dave March <dbmarch@gmail.com>
2025-11-16 13:39:06 -0500

    use model for 2 way

--- 06A-Custom2wayBinding/src/app/rect/rect.component.ts
+++ 06A-Custom2wayBinding/src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output} from '@angular/core';
+import { Component, input, output, model} from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -9,11 +9,13 @@ import { Component, input, output} from '@angular/core';
 })
 export class RectComponent {
   
-  size = input.required< {width: string; height: string}>();
-  sizeChange=output< {width: string; height: string}>();
+  // size = input.required< {width: string; height: string}>();
+  // sizeChange=output< {width: string; height: string}>();
+
+  size = model.required<{width: string; height: string}>();
 
   onReset() {
-    this.sizeChange.emit({
+    this.size.set({
       width: '200',
       height: '200'
     })
