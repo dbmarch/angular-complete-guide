73f13960cc2163a2506f40a1932be587bd8ae1cc
Dave March <dbmarch@gmail.com>
2025-11-26 22:57:51 -0500

    bring in signals

--- 11-rxjs/src/app/app.component.ts
+++ 11-rxjs/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, inject, OnInit, DestroyRef } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { Component, inject, OnInit, DestroyRef, signal, effect } from '@angular/core';
+import { interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +9,28 @@ import { interval, Subscription } from 'rxjs';
 export class AppComponent implements OnInit {
   private destroyRef = inject(DestroyRef);
 
-  ngOnInit(): void {
-    const subscription = interval(1000).subscribe({
-      next: (value) => console.log('Tick value:', value )
-  } )
 
-  this.destroyRef.onDestroy(() => {
-    subscription.unsubscribe();
-  });
+  constructor() {
+    effect(()=>{
+      console.log(`Clicked Button ${this.clickCount()}`)
+      });
+  }
+
+  clickCount = signal(0);
+
+  ngOnInit(): void {
+  //   const subscription = interval(1000).pipe(
+  //     map(value => value * 2)
+  //   ).subscribe({
+  //     next: (value) => console.log('Tick value:', value )
+  // } )
+
+  // this.destroyRef.onDestroy(() => {
+  //   subscription.unsubscribe();
+  // });
+}
+
+onClick() {
+  this.clickCount.update(value => value + 1);
 }
 }
