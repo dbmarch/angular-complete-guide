61ea64ae8b68cbfa1fb74c7261e691233cfc7700
Dave March <dbmarch@gmail.com>
2025-11-29 15:22:42 -0500

    More complex signup form

--- 12-forms-reactive/src/app/auth/signup/signup.component.ts
+++ 12-forms-reactive/src/app/auth/signup/signup.component.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
+  imports: [ReactiveFormsModule]
 })
-export class SignupComponent {}
+export class SignupComponent {
+  
+  form = new FormGroup ({
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email]
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)]
+    })
+  })
+
+  onSubmit() {
+    console.log ('onSubmit', this.form.value)
+    
+    if (this.form.invalid) {
+      return;
+    }
+    
+    this.form.reset();
+  }
+}
