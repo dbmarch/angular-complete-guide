a524d2f7d444c258d936d5ec4b6c3671d944ae22
Dave March <dbmarch@gmail.com>
2025-11-29 15:42:31 -0500

    tie in all fields and bring in css

--- 12-forms-reactive/src/app/auth/signup/signup.component.ts
+++ 12-forms-reactive/src/app/auth/signup/signup.component.ts
@@ -17,8 +17,38 @@ export class SignupComponent {
     }),
     password: new FormControl('', {
       validators: [Validators.required, Validators.minLength(6)]
+    }),
+    confirmPassword: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)]
+    }),
+    firstName: new FormControl('', {
+      validators: [Validators.required]
+    }),
+    lastName: new FormControl('', {
+      validators: [Validators.required]
+    }),
+    street: new FormControl('', {
+      validators: [Validators.required]
+    }),
+    number: new FormControl('', {
+      validators: [Validators.required]
+    }),
+    postalCode: new FormControl('', {
+      validators: [Validators.required]
+    }),
+    city: new FormControl('', {
+      validators: [Validators.required]
+    }),   
+    role: new FormControl<'student' | 'teacher' | 'employee' | 'founder' | 'other'> ('other', { 
+      validators: [Validators.required]
+    }),
+    acquisition: new FormControl('other', {
+      validators: [Validators.required]
+    }),
+    terms: new FormControl(false, {
+      validators: [Validators.requiredTrue]
     })
-  })
+  });
 
   onSubmit() {
     console.log ('onSubmit', this.form.value)
@@ -27,7 +57,9 @@ export class SignupComponent {
       return;
     }
   }
+
   onReset() {
+    console.log ("onReset");
     this.form.reset();
   }
 
