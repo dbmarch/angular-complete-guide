67baf63edf6238ce09291cf08386b04319f4e2fb
Dave March <dbmarch@gmail.com>
2025-12-03 22:24:03 -0500

    dynamic routes

--- 13-Routing/src/app/users/user-tasks/user-tasks.component.ts
+++ 13-Routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, input, computed} from '@angular/core';
+import { UsersService } from '../users.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
@@ -6,4 +8,25 @@ import { Component } from '@angular/core';
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {}
+export class UserTasksComponent {
+  // This will be set by angular router
+  userId = input.required<string>();
+  private usersService = inject(UsersService);
+  private activatedRoute = inject(ActivatedRoute);
+  userName = '';
+
+  // userName = computed(() => {
+  //   const user = this.usersService.users.find(user => user.id === this.userId());
+  //   return user ? user.name : 'Unknown User';
+  // });
+
+  ngOnInit(): void {
+    console.log(this.activatedRoute)
+    this.activatedRoute.paramMap.subscribe({
+      next: (paramMap) => { 
+        this.userName = this.usersService.users.find((u) => u.id === paramMap.get('userId'))?.name || ''
+      }
+    }
+    );
+  } 
+}
