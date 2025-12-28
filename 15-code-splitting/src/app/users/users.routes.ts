1806e7414e49899c0a0e6b2b34c38039c57a3a1b
Dave March <dbmarch@gmail.com>
2025-12-28 08:39:39 -0500

    deferrable

--- 15-code-splitting/src/app/users/users.routes.ts
+++ 15-code-splitting/src/app/users/users.routes.ts
@@ -2,25 +2,31 @@ import { Routes } from '@angular/router';
 
 import { TasksComponent, resolveUserTasks } from '../tasks/tasks.component';
 import { NewTaskComponent, canLeaveEditPage } from '../tasks/new-task/new-task.component';
+import { TasksService } from '../tasks/tasks.service'
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'tasks',
-    pathMatch: 'full',
-  },
-  {
-    path: 'tasks', // <your-domain>/users/<uid>/tasks
-    // component: TasksComponent,
-    loadComponent: () => import('../tasks/tasks.component').then((mod)=> mod.TasksComponent),
-    runGuardsAndResolvers: 'always',
-    resolve: {
-      userTasks: resolveUserTasks,
+    providers: [TasksService], 
+    children: [
+    {
+      path: '',
+      redirectTo: 'tasks',
+      pathMatch: 'full',
     },
-  },
-  {
-    path: 'tasks/new',
-    component: NewTaskComponent,
-    canDeactivate: [canLeaveEditPage]
-  },
+    {
+      path: 'tasks', // <your-domain>/users/<uid>/tasks
+      // component: TasksComponent,
+      loadComponent: () => import('../tasks/tasks.component').then((mod)=> mod.TasksComponent),
+      runGuardsAndResolvers: 'always',
+      resolve: {
+        userTasks: resolveUserTasks,
+      },
+    },
+    {
+      path: 'tasks/new',
+      component: NewTaskComponent,
+      canDeactivate: [canLeaveEditPage]
+    },
+  ]}
 ];
